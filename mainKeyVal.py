1ee2ebe77774e88690bc8055b8750bd078b6feb9
sanatsangamalli <ssangama@ucsc.edu>
2019-10-20 20:46:12 -0700

    testing all 3, test script included

--- mainKeyVal.py
+++ mainKeyVal.py
@@ -5,22 +5,30 @@ import sys
 class mainKeyVal:
 
 	def _init__():
-		self.dict = {}
+		self.dictionary = {}
 
-	def get(self, request):
-		return "This method is unsupported.", 405
+	def get(self, request, key_name):
+		if key_name in self.dictionary:
+			return jsonify({"doesExist":true, "message":"Retrieved successfully", "value":self.dictionary[key_name]}), 200
+		else:
+			return jsonify({"doesExist":false, "error:":"Key does not exist", "message":"Error in GET"}), 404
 		
 	def put(self, request, key_name):
-		#print(key_name)
-		#print(request.data)
 		if len(key_name) > 50:
 			return jsonify({"error:":"Key is too long", "message":"Error in PUT"}), 400
 		req_data = request.get_json()
 		if 'value' in req_data:
 			data = req_data['value']
-			#add to dict
+			replaced = key_name in self.dictionary
+			self.dictionary[key_name] = data	
+			return jsonify({"message":"Added successfully", "replaced":replaced}), 201
+			
 		else:
 			return jsonify({"error:":"Value is missing", "message":"Error in PUT"}), 400
 			
-	def delete(self, request):
-		return 405
+	def delete(self, request, key_name):
+		if key_name in self.dictionary:
+			del self.dictionary[key_name]
+			return jsonify({"doesExist":true, "message":"Deleted successfully"}), 200
+		else:
+			return jsonify({"doesExist":false, "error:":"Key does not exist", "message":"Error in DELETE"}), 404
