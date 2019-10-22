8f85a0cbcc7ee7f73f451aef5f2a684bb0b53569
Adit Bhagat <adbhagat@ucsc.edu>
2019-10-22 01:27:21 -0700

    Working follower

--- followerKeyVal.py
+++ followerKeyVal.py
@@ -12,7 +12,7 @@ class followerKeyVal:
                 response = requests.get('http://'+ os.environ['FORWARDING_ADDRESS'] + '/kv-store/' + key_name, data=json.dumps(reqdata), headers=dict(request.headers), timeout=20)
             else:
                 response = requests.get('http://'+ os.environ['FORWARDING_ADDRESS'] + '/kv-store/' + key_name, headers=dict(request.headers), timeout=20)
-        except (requests.Timeout, requests.exceptions.ConnectionError):
+        except:
             return jsonify({'error': 'Main instance is down', 'message': 'Error in GET'}), 503
         else:
             return response.content, response.status_code
@@ -21,10 +21,7 @@ class followerKeyVal:
         try:
             reqdata = request.get_json(silent=True)
             if reqdata is not None:
-                print(reqdata)
-                print(type(reqdata))
                 response = requests.put('http://'+ os.environ['FORWARDING_ADDRESS'] + '/kv-store/' + key_name, data=json.dumps(reqdata), headers=dict(request.headers), timeout=20)
-                print("Reqdata done")
             else:
                 response = requests.put('http://'+ os.environ['FORWARDING_ADDRESS'] + '/kv-store/' + key_name, headers=dict(request.headers), timeout=20)
         except:
